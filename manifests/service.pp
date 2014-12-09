c496dcd5ffb41ace2bee2cf638621f62105720ad
Chris Hayes <hayes.christopher@ericsson.com>
2014-12-09 00:12:17 -0500

    Syntax fixes, service.pp integration

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,10 +2,10 @@
 class seafile::service {
   exec { "seafile_sh":
     command => "seafile.sh start",
-    path    => $seafile::params::install_dir/seafile-server-latest,
-  }
+    path    => "${seafile::params::install_dir}/seafile-server-latest",
+  } ->
   exec { "seahub_sh":
     command => "seahub.sh start",
-    path    => $seafile::params::install_dir/seafile-server-latest,
+    path    => "${seafile::params::install_dir}/seafile-server-latest",
   }
 }
