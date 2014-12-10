e84365d427bb145dbe9dd02eb858e17bb614eba5
Chris Hayes <chayes@cactuspowered.com>
2014-12-09 21:13:34 -0500

    Established basic installation functionality in install.pp. Retooled url,folder,filename construction.

--- manifests/install.pp
+++ manifests/install.pp
@@ -24,33 +24,58 @@ define tarball($pkg_tgz, $module_name, $install_dir) {
 
 class seafile::install {
 # TODO - Find package solution for CentOS 7.
-  package { "python-setuptools":
-    ensure => "installed"
-  }
 
-  package { "python-imaging":
-    ensure => "installed"
-  }
+#  package { "python-setuptools":
+#    ensure => "installed"
+#  }
 
-  package { "python-simplejson":
-    ensure => "installed"
-  }
+#  package { "python-imaging":
+#    ensure => "installed"
+#  }
+
+#  package { "python-simplejson":
+#    ensure => "installed"
+#  }
 
   #exec { "install_python_deps":
   #  command => "sudo yum -y install sqlite python-simplejson python-setuptools python-imaging",
   #  refreshonly => true
 #} ->
 
-  # TODO - Find better method than a bash script for this.
-  file { "seafile_extract.sh":
-        ensure  => file,
-        mode    => '0755',
-        owner   => 'seafile',
-        content => template( "seafile/seafile_extract.sh.erb" )
-    }
+  # TODO - Move to top scope
+  $seafile_symlink = "${seafile::params::install_dir}/${seafile::params::foldername}"
 
-  #$location = "/opt/seafile"
-  #$pkg_name = "seafile"
+  file { "${seafile::params::install_dir}":
+    ensure => "directory",
+    owner  => "root",
+    group  => "wheel",
+    mode   => "0755",
+  } ->
+
+  # TODO - Find better method than a bash script to handle tarball.
+  file { "${seafile::params::install_dir}/seafile_extract.sh":
+    ensure  => file,
+    replace => true,
+    owner   => "root",
+    mode    => "0755",
+    content => template( "seafile/seafile_extract.sh.erb" )
+  } ->
+  
+  exec { 'extract_seafile':
+    require  => [
+      File['/opt/seafile/seafile_extract.sh'],
+    ],
+    # TODO - Fix to run from path.
+    command   => 'seafile_extract.sh',
+    path    => [$seafile::params::install_dir, "/usr/bin"],
+    }
+  
+  # Symlink for seafile server installation folder
+  # TODO Fix requirement for fully qualified name in file.
+  file { '/opt/seafile/seafile-server-latest':
+   ensure => 'link',
+   target => $seafile_symlink,
+  }
 
   #tarball { "$pkg_name":
   #  module_name => "foo",
